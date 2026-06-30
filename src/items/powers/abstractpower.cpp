e70b225b2f6169dd57c81da53cedce82ed0a6b30
atry <atry.2595@gmail.com>
2026-06-30 11:53:23 +0330

    fix amount between min&max in constructor

--- src/items/powers/abstractpower.cpp
+++ src/items/powers/abstractpower.cpp
@@ -5,6 +5,8 @@
 abstractPower::abstractPower(QString name_init, abstractEntity* owner_init, int x, int max_x, int min_x, bool buff_init, int priority_init)
     :name(name_init), owner(owner_init), amount(x), max_amount(max_x), min_amount(min_x), is_buff(buff_init), priority_order(priority_init)
 {
+    if (amount > max_amount) amount = max_amount;
+    if (amount < min_amount) amount = min_amount;
 }
 
 void abstractPower::increase(int value){
