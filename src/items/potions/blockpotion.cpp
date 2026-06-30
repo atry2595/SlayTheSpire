bb0a3da053aea1b5246a37cac04a0651e59f7a97
atry <atry.2595@gmail.com>
2026-06-30 14:06:11 +0330

    add block potion play function

--- src/items/potions/blockpotion.cpp
+++ src/items/potions/blockpotion.cpp
@@ -1,9 +1,15 @@
 #include "blockpotion.h"
+#include "combat/blocking_info.h"
 
 blockPotion::blockPotion(abstractEntity* owner_init)
     :abstractPotion(tr("Block Potion"), tr("Gain 12 Block."), owner_init)
 {}
 
 void blockPotion::play(playInfo& info){
+    blockingInfo b;
+    b.block = 12;
+    b.owner = owner;
+    b.affected_by_other = false;
 
+    info.actions.apply_block(b);
 }
