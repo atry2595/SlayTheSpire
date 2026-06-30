048056555ec9f0898cb31b91f1acc3639a2b8103
borna-ed <bornaelahidoost@gmail.com>
2026-06-30 20:54:19 +0330

    update stats.h

--- src/core/login/stats.h
+++ src/core/login/stats.h
@@ -1,13 +1,50 @@
 #ifndef STATS_H
 #define STATS_H
 
-#include <QObject>
+#include <QString>
+#include <QStringList>
 
-class stats
+class Stats
 {
-    Q_OBJECT
 public:
-    stats();
+    Stats();
+
+    int getScore() const;
+    int getMonstersKilled() const;
+    int getElitesKilled() const;
+    int getBossesKilled() const;
+    int getTotalEnemiesKilled() const;
+    int getHighestFloorReached() const;
+    int getTimesPlayed() const;
+    int getTimesWon() const;
+    int getTimesLost() const;
+    bool isSkinUnlocked(int skinIndex) const;
+
+    void addScore(int amount);
+    void addMonsterKill();
+    void addEliteKill();
+    void addBossKill();
+    void updateHighestFloor(int floor);
+    void incrementTimesPlayed();
+    void incrementTimesWon();
+    void incrementTimesLost();
+    void unlockSkin(int skinIndex);
+
+    QString toFileRecord() const;
+    static Stats fromFileRecord(const QString &record);
+
+private:
+    int score;
+    int monstersKilled;
+    int elitesKilled;
+    int bossesKilled;
+
+    bool skinUnlocked[5];
+
+    int highestFloorReached;
+    int timesPlayed;
+    int timesWon;
+    int timesLost;
 };
 
-#endif // STATS_H
+#endif
