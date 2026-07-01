1fd4a058a58008c995c130a982e417011ccf7cd4
atry <atry.2595@gmail.com>
2026-07-01 09:29:45 +0330

    add abstract power card & inflame power card
    * edit disarm first_energy

---
+++ src/cards/power/inflame.h
@@ -0,0 +1,29 @@
+#ifndef INFLAME_H
+#define INFLAME_H
+#include <QObject>
+#include "abstractpowercard.h"
+
+class inflame : public abstractPowerCard
+{
+    Q_OBJECT
+
+    int base_effect = 2;
+    int effect = 2;
+
+public:
+
+    inflame();
+    ~inflame() = default;
+
+    int get_effect() { return effect; }
+
+    QString generate_description() override;
+    void play(playInfo& play_info) override;
+    void upgrade() override;
+    void base_upgrade() override;
+
+    cardID get_card_id() override { return cardID::inflame; }
+    bool is_rare() override { return false; }
+};
+
+#endif // INFLAME_H
