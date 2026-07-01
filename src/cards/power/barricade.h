39ed568a89486427c6f5505850a4c6a85c429609
atry <atry.2595@gmail.com>
2026-07-01 11:20:22 +0330

    add barricade power card

---
+++ src/cards/power/barricade.h
@@ -0,0 +1,25 @@
+#ifndef BARRICADE_H
+#define BARRICADE_H
+#include <QObject>
+#include "abstractpowercard.h"
+
+class barricade : public abstractPowerCard
+{
+    Q_OBJECT
+
+public:
+
+    barricade();
+    ~barricade() = default;
+
+
+    QString generate_description() override;
+    void play(playInfo& play_info) override;
+    void upgrade() override;
+    void base_upgrade() override;
+
+    cardID get_card_id() override { return cardID::barricade; }
+    bool is_rare() override { return true; }
+};
+
+#endif // BARRICADE_H
