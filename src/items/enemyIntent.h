04bcf7741f75900ce70343ddcf5c8fbbb7e881a0
atry <atry.2595@gmail.com>
2026-07-01 14:12:37 +0330

    add intent ID and enemy_intent struct

---
+++ src/items/enemyIntent.h
@@ -0,0 +1,17 @@
+#ifndef ENEMYINTENT_H
+#define ENEMYINTENT_H
+
+#include <QString>
+#include "categories/intents.h"
+
+struct enemy_intent{
+
+    QString name;
+    intent_type type;
+    int damage = 0;
+    int repeat = 1;
+    bool hidden = false;
+
+};
+
+#endif // ENEMYINTENT_H
