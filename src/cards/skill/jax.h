3995c345710f656e50c59da07a18edca93afc14f
atry <atry.2595@gmail.com>
2026-07-01 12:20:40 +0330

    add J.A.X. skill card

---
+++ src/cards/skill/jax.h
@@ -0,0 +1,36 @@
+#ifndef JAX_H
+#define JAX_H
+#include <QObject>
+#include "abstractskillcard.h"
+
+class JAX : public abstractSkillCard
+{
+    Q_OBJECT
+
+protected:
+
+    int base_effect = 2;
+    int effect = 2;
+    int base_dmg = 3;
+    int dmg = 3;
+
+
+public:
+
+    JAX();
+    ~JAX() = default;
+
+    void combat_reset() override;
+
+    int get_effect() { return effect; }
+
+    QString generate_description() override;
+    void play(playInfo& play_info) override;
+    void upgrade() override;
+    void base_upgrade() override;
+
+    cardID get_card_id() override { return cardID::JAX; }
+    bool is_rare() override { return false; }
+};
+
+#endif // JAX_H
