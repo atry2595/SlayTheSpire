97e826dc77e81246d57675c78d314a96d64dbd3f
atry <atry.2595@gmail.com>
2026-07-01 11:44:53 +0330

    add slimed & wound status card

---
+++ src/cards/status/slimed.h
@@ -0,0 +1,25 @@
+#ifndef SLIMED_H
+#define SLIMED_H
+#include <QObject>
+#include "abstractstatuscard.h"
+
+class slimed : public abstractStatusCard
+{
+    Q_OBJECT
+
+public:
+
+    slimed();
+    ~slimed() = default;
+
+
+    QString generate_description() override;
+    void play(playInfo& play_info) override;
+    void upgrade() override;
+    void base_upgrade() override;
+
+    cardID get_card_id() override { return cardID::slimed; }
+    bool is_rare() override { return false; }
+};
+
+#endif // SLIMED_H
