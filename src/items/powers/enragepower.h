487b41dbc31e74d231978652bc834b4954e5e5d3
atry <atry.2595@gmail.com>
2026-07-05 13:54:01 +0330

    add enrage power

---
+++ src/items/powers/enragepower.h
@@ -0,0 +1,20 @@
+#ifndef ENRAGEPOWER_H
+#define ENRAGEPOWER_H
+
+#include "abstractpower.h"
+
+class enragePower : public abstractPower
+{
+    Q_OBJECT
+private:
+    QMetaObject::Connection play_card_connection;
+
+public:
+    enragePower(abstractEntity* owner_init, int x);
+    void added_time(game_action&) override;
+    void at_turn_end(game_action&) override;
+    QString generate_description() override;
+    powerID get_id() override { return powerID::enrage; }
+};
+
+#endif // ENRAGEPOWER_H
