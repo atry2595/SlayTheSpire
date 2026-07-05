369f1e6a64941c9d163fdcf6d13cfdc410a8b8f9
atry <atry.2595@gmail.com>
2026-07-05 16:14:07 +0330

    add painful stabs power

---
+++ src/items/powers/painfulstabpower.cpp
@@ -0,0 +1,30 @@
+#include "painfulstabpower.h"
+#include "entity/ironclad.h"
+#include "combat/blocking_info.h"
+#include "cards/cardfactory.h"
+
+painfulStabPower::painfulStabPower(abstractEntity* owner_init, int x)
+    : abstractPower(tr("Painful Stabs"), owner_init, x, 1, 0, true, 0)
+{
+    description = tr("Whenever you receive attack damage from this enemy, add 1 Wounds into your discard pile.");
+}
+
+
+QString painfulStabPower::generate_description(){
+    description = tr("Whenever you receive attack damage from this enemy, add 1 Wounds into your discard pile.");
+    return description;
+}
+
+void painfulStabPower::added_time(game_action& actions) {
+
+
+    add_card_connection = connect(actions.get_event(), &combatEvent::damage_applied, this,
+        [this](damageInfo& inf){
+        if (inf.attacker == owner && inf.damage >= 0){
+            abstractCard* nc = CardFactory::createCard(cardID::wound);
+            ironclad* player = dynamic_cast<ironclad*>(inf.target);
+            player->discard_pile_add(nc);
+        }
+    });
+
+}
