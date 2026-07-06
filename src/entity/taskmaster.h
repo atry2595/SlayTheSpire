225ddc03d12ca88f85a97d58d0212d74c3130128
atry <atry.2595@gmail.com>
2026-07-06 09:15:47 +0330

    add taskmaster

---
+++ src/entity/taskmaster.h
@@ -0,0 +1,26 @@
+#ifndef TASKMASTER_H
+#define TASKMASTER_H
+
+#include "abstractenemy.h"
+
+enum class taskmaster_move {
+    scouring_whip = 0
+};
+
+
+class taskmaster : public abstractEnemy
+{
+    Q_OBJECT
+
+    taskmaster(QString name_init, int base_max_hp_init);
+
+public:
+    static taskmaster* create(game_action& actions);
+    entityType get_type() override { return entityType::elite; }
+    entityID get_ID() override { return entityID::taskmaster ;}
+
+    void choose_move() override;
+    void play_turn(playInfo&)  override;
+};
+
+#endif // TASKMASTER_H
