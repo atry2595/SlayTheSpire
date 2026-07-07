e6d8c15ba15ebbba38f2811f472c7366df36ed27
atry <atry.2595@gmail.com>
2026-07-07 15:06:16 +0330

    add black star relic

---
+++ src/items/relics/black_star_relic.h
@@ -0,0 +1,20 @@
+#ifndef BLACK_STAR_RELIC_H
+#define BLACK_STAR_RELIC_H
+
+#include "abstractrelic.h"
+
+class black_star_relic : public abstractRelic
+{
+    Q_OBJECT
+    QMetaObject::Connection start_combat_connection;
+
+public:
+    black_star_relic(abstractEntity* owner_init);
+    QString generate_description() override;
+    relicID get_id() override { return relicID::black_star; }
+    relicRarity get_rarity() override { return relicRarity::boss; }
+
+    void added_time(game_action&) override;
+};
+
+#endif // BLACK_STAR_RELIC_H
