479a55efa82c0b2cab9e59bb2489190d882eddb0
borna-ed <bornaelahidoost@gmail.com>
2026-07-15 02:33:02 +0330

    Add ShopItem class

--- src/core/shop/shopitem.cpp
+++ src/core/shop/shopitem.cpp
@@ -1,3 +1,51 @@
-#include "shopitem.h"
+#include "ShopItem.h"
 
-ShopItem::ShopItem() {}
+ShopItem::ShopItem(ShopItemType type, int id, int price, const QString& name)
+    : m_type(type), m_id(id), m_price(price), m_name(name), m_isSale(false), m_isPurchased(false)
+{
+}
+
+ShopItemType ShopItem::getType() const
+{
+    return m_type;
+}
+
+int ShopItem::getId() const
+{
+    return m_id;
+}
+
+int ShopItem::getPrice() const
+{
+    return m_price;
+}
+
+void ShopItem::setPrice(int price)
+{
+    m_price = price;
+}
+
+QString ShopItem::getName() const
+{
+    return m_name;
+}
+
+bool ShopItem::isSale() const
+{
+    return m_isSale;
+}
+
+void ShopItem::setSale(bool sale)
+{
+    m_isSale = sale;
+}
+
+bool ShopItem::isPurchased() const
+{
+    return m_isPurchased;
+}
+
+void ShopItem::setPurchased(bool purchased)
+{
+    m_isPurchased = purchased;
+}
--- src/core/shop/shopitem.h
+++ src/core/shop/shopitem.h
@@ -1,13 +1,38 @@
 #ifndef SHOPITEM_H
 #define SHOPITEM_H
 
-#include <QObject>
+#include <QString>
+
+enum class ShopItemType {
+    Card,
+    Potion,
+    CardRemoval
+};
 
 class ShopItem
 {
-    Q_OBJECT
+private:
+    ShopItemType m_type;
+    int m_id;
+    int m_price;
+    QString m_name;
+    bool m_isSale;
+    bool m_isPurchased;
+
 public:
-    ShopItem();
+    ShopItem(ShopItemType type, int id, int price, const QString& name);
+
+    ShopItemType getType() const;
+    int getId() const;
+    int getPrice() const;
+    void setPrice(int price);
+    QString getName() const;
+
+    bool isSale() const;
+    void setSale(bool sale);
+
+    bool isPurchased() const;
+    void setPurchased(bool purchased);
 };
 
 #endif // SHOPITEM_H
