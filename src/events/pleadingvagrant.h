7da3f2fe350542ea232810f4361dc09ffec8ba59
borna-ed <bornaelahidoost@gmail.com>
2026-07-14 03:49:14 +0330

    add PleadingVagrant event

---
+++ src/events/pleadingvagrant.h
@@ -0,0 +1,26 @@
+#ifndef PLEADINGVAGRANT_H
+#define PLEADINGVAGRANT_H
+
+#include <QObject>
+
+#include "events/UnknownMap.h"
+#include "combat/game_action.h"
+#include "entity/ironclad.h"
+
+class PleadingVagrant
+{
+    Q_OBJECT
+
+private:
+    UnknownManager manager;
+
+public:
+    PleadingVagrant(game_action&, ironclad*);
+
+    const UnknownManager getManager() const
+    {
+        return manager;
+    }
+};
+
+#endif // PLEADINGVAGRANT_H
