7ea943a8d277049693b73f607f2b00e6eca3a6a9
borna-ed <bornaelahidoost@gmail.com>
2026-07-19 05:19:17 +0330

    add new font

---
+++ src/core/setting.h
@@ -0,0 +1,26 @@
+#ifndef SETTING_H
+#define SETTING_H
+
+#include <QString>
+
+enum class language{
+    English,
+    Persian
+};
+
+struct setting {
+    inline static language game_language = language::English;
+};
+
+
+namespace Fonts {
+inline QString Cascadia;
+inline QString koodak;
+inline QString titrZ;
+inline QString rock;
+inline QString cinema;
+inline QString lotrance;
+inline QString ebhaar;
+inline QString forte;
+}
+#endif // SETTING_H
